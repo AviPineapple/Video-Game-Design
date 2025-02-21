763294bcad1f4c71814e59c65a23ebad7f421180
AviPineapple <avneetk646@stemcollegiate.ca>
2025-02-21 08:55:07 -0700

    Create Feb_21.cs

---
+++ Feb_21.cs
@@ -0,0 +1,5 @@
+// "Old files did not save. I made a sentance using string, int, and double."
+string firstname = "Avneet";
+int number = 14;
+double temp = -60.5;
+Console.WriteLine($"Hello, {firstname}! You have {number} messages in your inbox. The temperature is {temp} celsius.");
