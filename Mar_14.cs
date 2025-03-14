f77d149eeb0e28bf8e539c0298964e7155f1e8ed
AviPineapple <avneetk646@stemcollegiate.ca>
2025-03-14 08:53:26 -0600

    Create Mar_14.cs

---
+++ Mar_14.cs
@@ -0,0 +1,6 @@
+//Module unit 3 and 4
+string firstName = "Avneet";
+string greeting = "Hello";
+string message = greeting + " " + firstName + "!";
+string projectName = "Avneet's-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
